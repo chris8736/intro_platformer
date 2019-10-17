935125a94c20eccffc36566e7c602910c9bc767f
Christopher Tam <christophertam@brandeis.edu>
2019-10-16 23:53:33 -0400

    placeholder 3d objects and world
    made simple 2d layout using 3d cubes
    wrote "dumb" script applying force upon key press

---
+++ Intro Platformer/Assets/Scripts/PlaceholderMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceholderMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 500 * Time.deltaTime, 0);
+        }
+    }
+}
