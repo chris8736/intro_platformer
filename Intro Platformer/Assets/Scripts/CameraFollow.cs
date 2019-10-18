0287d7548a99cb3aa1c9b70196cb2cfec1539e07
Anthony Fong <33325784+anthonygfong@users.noreply.github.com>
2019-10-18 18:35:28 -0400

    some 3D demo tricks
    camera follow
    physics
    input

---
+++ Intro Platformer/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.z = -10;
+        transform.position = newPosition;
+    }
+}
--- Intro Platformer/Assets/Scripts/PlaceholderMovement.cs
+++ Intro Platformer/Assets/Scripts/PlaceholderMovement.cs
@@ -6,21 +6,29 @@ public class PlaceholderMovement : MonoBehaviour
 {
 
     public Rigidbody rb;
+    private bool jump;
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (jump) return;
         if (Input.GetKey("d"))
         {
-            rb.AddForce(500 * Time.deltaTime, 0, 0);
+            rb.AddForce(1000 * Time.deltaTime, 0, 0);
         }
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+            rb.AddForce(-1000 * Time.deltaTime, 0, 0);
         }
         if (Input.GetKey("w"))
         {
-            rb.AddForce(0, 500 * Time.deltaTime, 0);
+            jump = true;
+            rb.AddForce(new Vector3(0, 400 * Time.deltaTime, 0), ForceMode.Impulse);
         }
+    }
+
+    void OnCollisionEnter()
+    {
+        jump = false;
     }
 }
