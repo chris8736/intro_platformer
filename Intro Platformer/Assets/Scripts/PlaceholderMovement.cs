0f3ab90bced9e7af8cb95189a09e9da13c191e7c
Christopher Tam <christophertam@brandeis.edu>
2019-10-25 16:47:41 -0400

    collision tilemap fix

--- Intro Platformer/Assets/Scripts/PlaceholderMovement.cs
+++ Intro Platformer/Assets/Scripts/PlaceholderMovement.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 public class PlaceholderMovement : MonoBehaviour
 {
 
-    public Rigidbody rb;
+    public Rigidbody2D rb;
 
     // Update is called once per frame
     void FixedUpdate()
     {
         if (Input.GetKey("d"))
         {
-            rb.AddForce(500 * Time.deltaTime, 0, 0);
+            rb.AddForce(new Vector2(500 * Time.deltaTime, 0));
         }
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+            rb.AddForce(new Vector2(-500 * Time.deltaTime, 0));
         }
         if (Input.GetKey("w"))
         {
-            rb.AddForce(0, 500 * Time.deltaTime, 0);
+            rb.AddForce(new Vector2(0, 500 * Time.deltaTime));
         }
     }
 }
---
+++ Intro Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    private bool jump;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(new Vector2(1000 * Time.deltaTime, 0));
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(new Vector2(-1000 * Time.deltaTime, 0));
+        }
+        if (Input.GetKey("w") && !jump)
+        {
+            jump = true;
+            rb.AddForce(new Vector2(0, 400 * Time.deltaTime), ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionEnter2D()
+    {
+        jump = false;
+    }
+}
